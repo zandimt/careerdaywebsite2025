f4bbc186a4ff8194b0081c4bfceb9000ddb55ad9
squidistaken <marcusper2003@gmail.com>
2025-03-05 21:18:59 +0100

    refactor

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <title>My Next.js App</title>
+                <title>Cover Career Day</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from 'react';
 import { CareerDayLogo } from './components/logos';
-import Footer from './components/footer';
+import Footer from './components/footer/footer';
 
 interface TimeLeft {
   days: number;
@@ -10,65 +10,64 @@ interface TimeLeft {
   seconds: number;
 }
 
+const calculateTimeLeft = (): TimeLeft => {
+  const targetDate = new Date('2025-05-14T09:15:00');
+  const now = new Date();
+  const difference = targetDate.getTime() - now.getTime();
+
+  if (difference > 0) {
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / (1000 * 60)) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  } else {
+    return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  }
+};
+
 const App: React.FC = () => {
   const [timeLeft, setTimeLeft] = useState<TimeLeft>(calculateTimeLeft());
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const interval = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
 
-    return () => clearTimeout(timer);
-  });
-
-  function calculateTimeLeft(): TimeLeft {
-    const targetDate = new Date('2025-05-14T09:15:00'); // Set your target date here
-    const now = new Date();
-    const difference = targetDate.getTime() - now.getTime();
-
-    if (difference > 0) {
-      return {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    } else {
-      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-    }
-  }
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 pt-20">
-        <CareerDayLogo/>
-        <h1 className="text-4xl font-bold mb-8 text-gray-600">14 May 2025, Energy Academy</h1>
-        <div className="flex space-x-4">
-          <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
-            <span className="text-3xl font-bold text-gray-600">{timeLeft.days}</span>
-            <span className="text-gray-600">Days</span>
+      <div className="app-container">
+        <div className="flex flex-col items-center justify-center flex-grow bg-gray-100 pt-20">
+          <CareerDayLogo />
+          <h1 className="text-4xl font-bold mb-8 text-gray-600">14 May 2025, Energy Academy</h1>
+          <div className="flex space-x-4">
+            <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
+              <span className="text-3xl font-bold text-gray-600">{timeLeft.days}</span>
+              <span className="text-gray-600">Days</span>
+            </div>
+            <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
+              <span className="text-3xl font-bold text-gray-600">{timeLeft.hours}</span>
+              <span className="text-gray-600">Hours</span>
+            </div>
+            <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
+              <span className="text-3xl font-bold text-gray-600">{timeLeft.minutes}</span>
+              <span className="text-gray-600">Minutes</span>
+            </div>
+            <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
+              <span className="text-3xl font-bold text-gray-600">{timeLeft.seconds}</span>
+              <span className="text-gray-600">Seconds</span>
+            </div>
           </div>
-          <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
-            <span className="text-3xl font-bold text-gray-600">{timeLeft.hours}</span>
-            <span className="text-gray-600">Hours</span>
-          </div>
-          <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
-            <span className="text-3xl font-bold text-gray-600">{timeLeft.minutes}</span>
-            <span className="text-gray-600">Minutes</span>
-          </div>
-          <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
-            <span className="text-3xl font-bold text-gray-600">{timeLeft.seconds}</span>
-            <span className="text-gray-600">Seconds</span>
-          </div>
+          <h2 className="text-3xl items-center font-bold mb-8 text-gray-600" style={{ padding: '40px' }}>
+            <a href="https://www.svcover.nl/events/4695">Register your interest!</a>
+          </h2>
         </div>
-        <h2 className="text-3xl items-center font-bold mb-8 text-gray-600" style={registrationStyle}>
-          <a href="https://www.svcover.nl/events/4695">Register your interest!</a></h2>
-        <Footer/>
+        <Footer />
       </div>
   );
 };
-
-const registrationStyle = {
-  padding: '20px',
-}
 
 export default App;
